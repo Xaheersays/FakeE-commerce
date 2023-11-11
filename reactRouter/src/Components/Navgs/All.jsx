1f702580265ae5e866bd86ece6af9d128b9fb630
Xaheersays <shzaheer514@gmail.com>
2023-11-11 15:28:04 +0530

    all component done

--- reactRouter/src/Components/Navgs/All.jsx
+++ reactRouter/src/Components/Navgs/All.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Template from './Template'
 
 
 export default function All(){
+    // i am getting array of 20 objects
+    const [data,setData] = useState([])
     const url = "https://fakestoreapi.com/products";
-    fetch(url).then(res=>res.json()).then((data)=>{
-        console.log(data)
-    })
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then((data)=>{
+            setData(data)
+            console.log(data)
+        })
+        .catch(error=>console.error(error));
+    },[])
+    
+
+    const data_arr = useMemo(() => {
+        return data.map((data_obj) => (
+          <Template
+            key={data_obj.id}
+            image={data_obj.image}
+            title={data_obj.title}
+            price={data_obj.price}
+            rate={data_obj.rating.rate}
+            count={data_obj.rating.count}
+          />
+        ));
+      }, [data]);
+
     return (
-        <Template />
+        <>
+        <div className="border-2 flex flex-wrap w-screen   justify-around md:p-3 gap-3 p-4">
+            {data_arr}
+        </div>
+        </>
     )
 }
--- reactRouter/src/Components/Navgs/Template.jsx
+++ reactRouter/src/Components/Navgs/Template.jsx
@@ -1,39 +1,43 @@
 import application from '../../assets/application-store.gif'
-
-
 import React from "react";
+
+
+
 export default function Template(props){
-    const {image,title,price} = props
+    const {image,title,price,rate,count} = props
     // rating:{rate,count}
     return(
         <>
-        Template
+
         {/* outer div */}
-        <div className='border-2 max-w-sm flex flex-col items-center p-4 bg-violet-200 text-black gap-4
-        rounded-2xl cursor-pointer
-        ' >
+        <div className=' md:w-1/4  flex flex-col items-center p-4 bg-gray-200 text-black md:gap-4
+        rounded-2xl cursor-pointer w-full shadow-lg  
+        ' 
+        style={{ boxShadow: '0 4px 8px rgba(255, 255, 255, 0.8)', opacity: 0.92 }}
+        >
+
             {/* image div */}
             <div className=''>
-                <img className='h-80 w-auto rounded-2xl' src={application} alt="" />
+                <img className='h-80 w-auto rounded-2xl' src={image} alt="" />
             </div>
 
             {/* title */}
             <div>
-                <h4 className='text-2xl text-center font-semibold' >
-                    {"title djkfk kjdakjd  lfkalkd ajdkjad adjksadks"}
+                <h4 className='md:text-2xl text-center font-semibold ' >
+                    {title}
                 </h4>
             </div>
             {/* price */}
-            <div className='font-bold text-red-700'>
+            <div className='font-bold text-red-600'>
                 <p>
-                price : {"price"} &#x20B9;
+                price : {price} &#x20B9;
                 </p>
             </div>
             {/* ratings */}
             <div className='flex flex-col items-center cursor-default'>
-                <p className='text-sm'>Ratings : {4} &#9733;</p>
+                <p className='text-sm'>Ratings : {rate} &#9733;</p>
                 <p className='font-light '
-                >Total Ratings: {120}</p>
+                >Total Ratings: {count}</p>
             </div>
 
         </div>
